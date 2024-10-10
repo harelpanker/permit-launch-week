00e3f86b6b681fa9870837ce21369a018fd41386
Harel Panker <harelpanker@gmail.com>
2024-10-10 20:21:52 +0200

    Updating mobile UI

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,6 @@ import Image from 'next/image';
 import localFont from 'next/font/local';
 import styles from './styles.module.css';
 
-import logo from '@/app/assets/logo.svg';
-import logoCenter from '@/app/assets/logo-center.svg';
 import comp1 from '@/app/assets/comp1.svg';
 import comp2 from '@/app/assets/comp2.svg';
 import comp3 from '@/app/assets/comp3.svg';
@@ -23,6 +21,7 @@ import word2 from '@/app/assets/word1.svg';
 import word3 from '@/app/assets/word3.svg';
 import date from '@/app/assets/date.svg';
 import Marquee from './Marquee';
+import Logos from './Logos';
 
 interface TimeLeft {
   days: number;
@@ -96,221 +95,139 @@ const Page = () => {
 
   return (
     <section
-      className={`${styles.section} relative min-h-screen px-4 pb-32 pt-[270px] sm:pt-[320px] lg:px-12`}>
+      className={`${styles.section} relative flex min-h-screen flex-col gap-y-[100px] px-4 pb-32 pt-[270px] sm:pt-[320px] lg:px-12`}>
       {/* logo */}
-      <Image
-        priority
-        src={logo}
-        alt='logo'
-        className='absolute left-1/2 top-14 -translate-x-1/2 lg:left-[10%] lg:top-[74px] lg:translate-x-0'
-      />
-      <Image
-        priority
-        src={logoCenter}
-        alt='logo'
-        className='absolute left-1/2 top-[120px] max-w-[70%] -translate-x-1/2 lg:top-[46px]'
-      />
+      <Logos />
 
-      <div className='container relative z-20 text-[#3A2F2F]'>
-        <div className='content flex flex-col gap-y-12 sm:gap-y-32'>
-          {/* counter */}
-          <div className='relative z-10 mx-auto flex w-full max-w-[92%] flex-col sm:flex-row sm:items-center'>
-            <div className='flex'>
-              {/* days */}
-              <div className='item relative z-10'>
-                <div className='absolute z-20 flex h-full w-full items-center justify-center'>
-                  <div className='relative -left-[26px] bottom-8 flex'>
-                    <span
-                      className={`${helvetica.className} relative -left-[10px] text-[110px] tracking-[-0.15em] lg:leading-none`}>
-                      {days}
-                    </span>
-                    <span
-                      className={`${times.className} relative -left-3 bottom-[32px] self-end text-[36px] leading-none lg:text-3xl`}>
-                      d
-                    </span>
-                  </div>
-                </div>
-
-                <div className='relative z-10'>
-                  <Image
-                    src={comp1}
-                    alt=''
-                    priority
-                    className='hidden lg:block'
-                  />
-                  <Image
-                    src={comp1M}
-                    alt=''
-                    priority
-                    className='relative z-20 max-w-[186px] lg:hidden'
-                  />
-                  <Image
-                    src={star1}
-                    alt=''
-                    priority
-                    className={`absolute -left-8 -top-12 max-w-[140px] pt-1 lg:hidden ${styles.spin}`}
-                  />
-                </div>
-              </div>
-              {/* hours */}
-              <div className='item relative -left-[76px] top-[44px] z-20 sm:-left-7 sm:top-4'>
-                <div className='absolute z-20 flex h-full w-full items-center justify-center'>
-                  <div className='relative -left-[30px] bottom-[50px] flex'>
-                    <span
-                      className={`${helvetica.className} relative -left-[20px] text-[110px] tracking-[-0.15em] lg:leading-none`}>
-                      {hours}
-                    </span>
-                    <span
-                      className={`${times.className} relative bottom-[50px] self-end text-[36px] leading-none lg:text-3xl`}>
-                      h
-                    </span>
-                  </div>
-                </div>
-
-                <div className='relative z-10'>
-                  <Image
-                    src={comp2}
-                    alt=''
-                    priority
-                    className='hidden lg:block'
-                  />
-                  <Image
-                    src={comp2M}
-                    alt=''
-                    priority
-                    className='relative z-10 max-w-[230px] lg:hidden'
-                  />
-                  <Image
-                    src={date}
-                    alt=''
-                    className='absolute -left-8 -top-10 z-20'
-                  />
-                </div>
-              </div>
+      <div className='relative z-10 mx-auto h-[386px] w-[340px] text-[#3A2F2F] md:hidden'>
+        {/* days */}
+        <div className='absolute left-4 top-0 z-10 w-full max-w-[156px]'>
+          <div className='absolute inset-0 z-30 flex h-full w-full items-center justify-center'>
+            <div className='relative left-[-20px]'>
+              <span
+                className={`${helvetica.className} text-[92px] tracking-[-0.05em]`}>
+                {days}
+              </span>
+              <span
+                className={`${times.className} relative left-0 top-2 text-[30px]`}>
+                d
+              </span>
             </div>
-            <div className='flex'>
-              {/* minutes */}
-              <div className='item relative -left-[15px] -top-[30px] z-40 sm:-left-32 sm:top-10'>
-                <div className='absolute z-20 flex h-full w-full items-center justify-center'>
-                  <div className='relative -left-[6px] flex'>
-                    <span
-                      className={`${helvetica.className} relative -left-[20px] bottom-10 text-[110px] tracking-[-0.05em] lg:leading-none`}>
-                      {minutes}
-                    </span>
-                    <span
-                      className={`${times.className} relative -left-3 bottom-24 self-end text-[36px] leading-none lg:text-3xl`}>
-                      m
-                    </span>
-                  </div>
-                </div>
+          </div>
+          <Image src={comp1M} alt='' priority className='relative z-20' />
+          <Image
+            src={star1}
+            alt=''
+            priority
+            className={`absolute -left-10 -top-12 z-10 max-w-[140px] pt-1 ${styles.spin}`}
+          />
+        </div>
+        {/* hours */}
+        <div className='absolute right-0 top-6 z-20 w-full max-w-[190px]'>
+          <div className='absolute inset-0 z-30 flex h-full w-full items-center justify-center'>
+            <div className='relative bottom-5 left-[-20px]'>
+              <span
+                className={`${helvetica.className} text-[92px] tracking-[-0.05em]`}>
+                {hours}
+              </span>
+              <span
+                className={`${times.className} relative -top-2 left-0 text-[30px]`}>
+                h
+              </span>
+            </div>
+          </div>
+          <Image src={comp2M} alt='' priority className='relative z-20' />
+          <Image
+            src={date}
+            alt=''
+            className='absolute -left-4 -top-6 z-40 -rotate-6'
+          />
+        </div>
+        {/* days */}
+        <div className='absolute bottom-4 left-0 z-40 w-full max-w-[186px]'>
+          <div className='absolute inset-0 z-30 flex h-full w-full items-center justify-center'>
+            <div className='relative left-[-20px]'>
+              <span
+                className={`${helvetica.className} text-[92px] tracking-[-0.05em]`}>
+                {minutes}
+              </span>
+              <span
+                className={`${times.className} relative -top-2 left-0 text-[30px]`}>
+                m
+              </span>
+            </div>
+          </div>
+          <Image src={comp3M} alt='' priority className='relative z-20' />
+          <Image src={word1} alt='' priority className='pt-1 lg:hidden' />
+        </div>
+        {/* hours */}
+        <div className='absolute bottom-0 right-3 z-20 w-full max-w-[160px]'>
+          <div className='absolute inset-0 z-30 flex h-full w-full items-center justify-center'>
+            <div className='relative bottom-5 left-[10px]'>
+              <span
+                className={`${helvetica.className} text-[92px] tracking-[-0.05em]`}>
+                {seconds}
+              </span>
+              <span
+                className={`${times.className} relative -left-4 top-6 text-[30px]`}>
+                s
+              </span>
+            </div>
+          </div>
+          <Image src={comp4M} alt='' priority className='relative z-20' />
+          <div className='flex w-full justify-end pt-1 lg:hidden'>
+            <Image src={word3} alt='' priority />
+          </div>
+        </div>
+      </div>
+      {/* button */}
+      <div className='relative z-20 flex items-center justify-center'>
+        <div className='relative'>
+          {/* button */}
+          <a
+            className={`${styles.button} relative z-30 rounded-full text-lg font-bold lg:text-3xl`}
+            target='_blank'
+            rel='noopener noreferrer'
+            href='https://app.permit.io'>
+            Register with GitHub
+          </a>
 
-                <Image
-                  src={comp3}
-                  alt=''
-                  priority
-                  className='hidden lg:block'
-                />
-                <div className='relative z-10'>
-                  <Image
-                    src={comp3M}
-                    alt=''
-                    priority
-                    className='max-w-[224px] lg:hidden'
-                  />
-                  <Image
-                    src={word1}
-                    alt=''
-                    priority
-                    className='pt-1 lg:hidden'
-                  />
-                </div>
-              </div>
-              {/* seconds */}
-              <div className='item relative -left-[94px] -top-[60px] z-30 sm:-left-[12rem] sm:top-2'>
-                <div className='absolute z-20 flex h-full w-full items-center justify-center'>
-                  <div className='relative -left-[40px] bottom-[40px] flex'>
-                    <span
-                      className={`${helvetica.className} relative left-[80px] text-[100px] tracking-[-0.05em] lg:leading-none`}>
-                      {seconds}
-                    </span>
-                    <span
-                      className={`${times.className} relative bottom-0 left-[70px] self-end text-[36px] leading-none lg:text-3xl`}>
-                      s
-                    </span>
-                  </div>
-                </div>
-
-                <Image
-                  src={comp4}
-                  alt=''
-                  priority
-                  className='hidden lg:block'
-                />
-                <div className='relative z-10'>
-                  <Image
-                    src={comp4M}
-                    alt=''
-                    priority
-                    className='max-w-[192px] lg:hidden'
-                  />
-                  <div className='flex w-full justify-end pt-1 lg:hidden'>
-                    <Image src={word3} alt='' priority />
-                  </div>
-                </div>
-              </div>
+          {/* swag */}
+          <div
+            className={`${times.className} absolute -right-[15px] -top-[200px] z-20 flex size-[150px] items-center justify-center text-center text-[16px] italic`}>
+            <div
+              style={{ lineHeight: 1.2 }}
+              className='absolute inset-0 z-20 flex items-center justify-center '>
+              A chance to
+              <br />
+              win a custom
+              <br />
+              keyboard!
+            </div>
+            <div className={`${styles.spin2}`}>
+              <Image src={star1} alt='' />
             </div>
           </div>
 
-          {/* button */}
-          <div className='relative z-20 flex items-center justify-center'>
-            <div className='relative'>
-              {/* button */}
-              <a
-                className={`${styles.button} relative z-20 rounded-full text-lg font-bold lg:text-3xl`}
-                target='_blank'
-                rel='noopener noreferrer'
-                href='https://app.permit.io'>
-                Register with GitHub
-              </a>
+          {/* keyboard */}
+          <Image
+            src={keyboardM}
+            alt=''
+            className='absolute bottom-10 left-0 z-10 -rotate-12 sm:w-[260px] sm:max-w-none lg:hidden'
+          />
 
-              {/* swag */}
-              <div
-                className={`${times.className} absolute -right-[15px] -top-[200px] z-20 flex size-[150px] items-center justify-center text-center text-[16px] italic`}>
-                <div
-                  style={{ lineHeight: 1.2 }}
-                  className='absolute inset-0 z-20 flex items-center justify-center '>
-                  A chance to
-                  <br />
-                  win a custom
-                  <br />
-                  keyboard!
-                </div>
-                <div className={`${styles.spin2}`}>
-                  <Image src={star1} alt='' />
-                </div>
-              </div>
-
-              {/* keyboard */}
-              <Image
-                src={keyboardM}
-                alt=''
-                className='absolute bottom-10 left-0 -rotate-12 '
-              />
-
-              {/* swag */}
-              <div
-                className={`${times.className} absolute -right-[75px] top-[10px] flex size-[170px] items-center justify-center text-center text-[16px] font-bold italic text-white`}>
-                <div className='absolute inset-0 z-20 flex items-center justify-center '>
-                  Cool SWAG
-                  <br />
-                  for EVERY
-                  <br />
-                  attendee
-                </div>
-                <div className={`${styles.spin}`}>
-                  <Image className={``} src={star2} alt='' />
-                </div>
-              </div>
+          {/* swag */}
+          <div
+            className={`${times.className} absolute -right-[75px] top-[10px] flex size-[170px] items-center justify-center text-center text-[16px] font-bold italic text-white`}>
+            <div className='absolute inset-0 z-20 flex items-center justify-center '>
+              Cool SWAG
+              <br />
+              for EVERY
+              <br />
+              attendee
+            </div>
+            <div className={`${styles.spin}`}>
+              <Image src={star2} alt='' />
             </div>
           </div>
         </div>
